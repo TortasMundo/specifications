d203c210c64938fbb029df438d05e97dcd145e01
alayor <alayor3@gmail.com>
2018-09-17 17:47:57 -0600

    Add step for order placing by order taker.

--- features/order.feature
+++ features/order.feature
@@ -1,5 +1,6 @@
 Feature: Get Orders
 
   Scenario: Get Last 50 Orders
+    Given Order Taker places an order
     When Kitchen sends request to get last orders
     Then Kitchen should receive successful response
--- steps/orders.js
+++ steps/orders.js
@@ -3,6 +3,10 @@ const { Given, When, Then } = require('cucumber')
 const GetOrdersRequest = require('support/web/requests/kitchen-api/orders/list')
 const { expect } = require('chai')
 
+Given('Order Taker places an order', async function () {
+
+});
+
 When('Kitchen sends request to get last orders', async function() {
   const request = new GetOrdersRequest.Builder().build()
   await this.send(request)
