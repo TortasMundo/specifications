0a1db66fc47288576f2f40b043513c66259d887a
alayor <alayor3@gmail.com>
2018-09-18 07:40:31 -0600

    Expect Kitchen to receive order just as was placed.

--- features/order.feature
+++ features/order.feature
@@ -4,4 +4,4 @@ Feature: Get Orders
     Given Order Taker places an order with 1 jamon
     When Kitchen sends request to get last orders
     Then Kitchen should receive successful response
-    And Kitchen should receive one order
+    And Kitchen should receive one order with 1 jamon, 0 lomo, 0 especial and 0 refrescos
--- steps/orders.js
+++ steps/orders.js
@@ -1,21 +1,26 @@
 const R = require('ramda')
-const {Given, When, Then} = require('cucumber')
+const { Given, When, Then } = require('cucumber')
 const GetOrdersRequest = require('support/web/requests/kitchen-api/orders/list')
 const PlaceOrderRequest = require('support/web/requests/order-taker-api/orders/place')
-const {expect} = require('chai')
+const { expect } = require('chai')
 
-Given('Order Taker places an order with {int} jamon', async function (jamon) {
-  const request = new PlaceOrderRequest.Builder()
-    .withJamon(jamon)
-    .build()
+Given('Order Taker places an order with {int} jamon', async function(jamon) {
+  const request = new PlaceOrderRequest.Builder().withJamon(jamon).build()
   await this.send(request)
 })
 
-When('Kitchen sends request to get last orders', async function () {
+When('Kitchen sends request to get last orders', async function() {
   const request = new GetOrdersRequest.Builder().build()
   await this.send(request)
 })
 
-Then('Kitchen should receive one order', function () {
-  expect(this.lastResponse.data.length).to.eql(1)
-})
+Then(
+  'Kitchen should receive one order with {int} jamon, {int} lomo, {int} especial and {int} refrescos',
+  function(jamon, lomo, especial, refrescos) {
+    expect(this.lastResponse.data.length).to.eql(1)
+    expect(this.lastResponse.data[0].jamon).to.eql(jamon)
+    expect(this.lastResponse.data[0].lomo).to.eql(lomo)
+    expect(this.lastResponse.data[0].especial).to.eql(especial)
+    expect(this.lastResponse.data[0].refrescos).to.eql(refrescos)
+  },
+)
