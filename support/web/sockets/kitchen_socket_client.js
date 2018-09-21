f86ffd96c3b15c3b5e008831e23db2325cb3766c
alayor <alayor3@gmail.com>
2018-09-21 08:10:48 -0600

    Send is-test as true in socket handshake.

--- support/web/sockets/kitchen_socket_client.js
+++ support/web/sockets/kitchen_socket_client.js
@@ -2,7 +2,7 @@ const config = require('config')
 let socket
 
 const create = () => {
-  const io = require('socket.io-client')
+  const io = require('socket.io-client', { query: "is-test=true" })
   socket = io(config.kitchen_api_host)
   return socket
 }
